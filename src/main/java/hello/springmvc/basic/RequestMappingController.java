917f951c5f1b6c031b18398ce17d5f3fb89d6870
pasly0920 <inwoo920621@gmail.com>
2023-01-25 18:16:14 +0900

    feat: request param 사용 예시

---
+++ src/main/java/hello/springmvc/basic/RequestMappingController.java
@@ -0,0 +1,25 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Slf4j
+@Controller
+public class RequestMappingController {
+
+    /**
+     * @RequestParam 사용 HTTP 파라미터 이름이 변수 이름과 같으면 @RequestParam(name="xx") 생략 가능
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(
+            @RequestParam String username,
+            @RequestParam int age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+}
