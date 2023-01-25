897a80e4c7d832742669b251a96eda07ae78b13d
pasly0920 <inwoo920621@gmail.com>
2023-01-25 18:22:28 +0900

    feat: model attribute 사용 예시

--- src/main/java/hello/springmvc/basic/RequestMappingController.java
+++ src/main/java/hello/springmvc/basic/RequestMappingController.java
@@ -2,6 +2,7 @@ package hello.springmvc.basic;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,5 +22,14 @@ public class RequestMappingController {
         log.info("username={}, age={}", username, age);
         return "ok";
     }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(),
+                helloData.getAge());
+        return "ok";
+    }
+
 
 }
