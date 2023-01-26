c65d3fdb5cb78658de6c6e7e44999849a5768880
pasly0920 <inwoo920621@gmail.com>
2023-01-26 10:53:35 +0900

    feat: request body 사용 예시

---
+++ src/main/java/hello/springmvc/basic/RequestBodyJsonController.java
@@ -0,0 +1,20 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Slf4j
+@Controller
+public class RequestBodyJsonController {
+
+    @ResponseBody
+    @PostMapping("/request-body-json-v3")
+    public String requestBodyJsonV3(@RequestBody HelloData data) {
+        log.info("username={}, age={}", data.getUsername(), data.getAge());
+        return "ok";
+    }
+
+}
